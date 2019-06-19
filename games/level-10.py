416b866b726f822c2b58f483db62f6d8ab3b49b1
Quinn <quinn.huang@oocl.com>
2019-06-19 12:43:26 +0800

    level 10 pass

--- games/level-10.py
+++ games/level-10.py
@@ -1,3 +1,37 @@
+import re
+from collections import namedtuple
 
 LEVEL = 10
 page_url = "http://www.pythonchallenge.com/pc/return/bull.html"
+
+# hint: a = [1, 11, 21, 1211, 111221,
+hint = [1, 11, 21, 1211, 111221]
+regex = re.compile(r"(\d)(\1*)")
+
+for item in hint:
+    piece = regex.findall(str(item))
+    parts = [str(len(i + j)) + i for i, j in piece]
+    print("".join(parts))
+
+# 312211
+# (3,)(1,)(2,2)(1,1)
+# 13112221
+
+# 13112221
+# (1,)(3,)(1,1)(2,22)(1,)
+# 1113213211
+print("--------------------")
+
+Item = namedtuple("Item", ("string", "length"))
+first = Item("1", 1)
+hint = [first]
+cur = "1"
+while len(hint) < 31:
+    piece = regex.findall(str(cur))
+    parts = [str(len(i + j)) + i for i, j in piece]
+    cur = "".join(parts)
+    hint.append(Item(cur, len(cur)))
+
+print(hint[30].length)
+
+
---
+++ games/level-11.py
@@ -0,0 +1,4 @@
+import re
+
+LEVEL = 11
+page_url = "http://www.pythonchallenge.com/pc/return/5808.html"
